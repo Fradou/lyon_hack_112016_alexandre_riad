284d3dd2e9b1bd75912dc92b2a4881985e79ed52
Alexandre FRADET <a_fradet@yahoo.fr>
2016-11-09 19:35:01 +0100

    Create method to fill database. Work wonder !

--- src/GeoBundle/Controller/DefaultController.php
+++ src/GeoBundle/Controller/DefaultController.php
@@ -2,7 +2,9 @@
 
 namespace GeoBundle\Controller;
 
+use GeoBundle\Entity\Location;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Doctrine\ORM\EntityManager;
 
 class DefaultController extends Controller
 {
@@ -10,4 +12,38 @@ class DefaultController extends Controller
     {
         return $this->render('GeoBundle:Default:index.html.twig');
     }
+
+    public function loadentityAction()
+    {
+        $recup=file_get_contents($this->get('kernel')->getRootDir()."/../web/lyondata.json");
+        $data = json_decode($recup, true);
+        $sites = $data["features"];
+        $em = $this->getDoctrine()->getManager();
+        foreach ($sites as $site) {
+            $localisation = new Location();
+            $localisation->setType($site['properties']['type']);
+            $localisation->setTypeDetail($site['properties']['type_detail']);
+            $localisation->setName($site['properties']['nom']);
+            $localisation->setAddress($site['properties']['adresse']);
+            $localisation->setPostalcode($site['properties']['codepostal']);
+            $localisation->setTown($site['properties']['commune']);
+            $localisation->setPhone($site['properties']['telephone']);
+            $localisation->setMail($site['properties']['email']);
+            $localisation->setWebsite($site['properties']['siteweb']);
+            $localisation->setFacebook($site['properties']['facebook']);
+            $localisation->setRank($site['properties']['classement']);
+            $localisation->setOpenhour($site['properties']['ouverture']);
+            $localisation->setRateclear($site['properties']['tarifsenclair']);
+            $localisation->setMinrate($site['properties']['tarifsmin']);
+            $localisation->setMaxrate($site['properties']['tarifsmax']);
+            $localisation->setProducer($site['properties']['producteur']);
+            $localisation->setLatitude($site['geometry']['coordinates'][1]);
+            $localisation->setLongitude($site['geometry']['coordinates'][0]);
+
+            $em->persist($localisation);
+        }
+            $em->flush();
+            return $this->render('GeoBundle:Default:index.html.twig');
+
+        }
 }
